ad622285457c6039c8d479868f15a8661e34c8bd
Jackson Isiko <isijackson@gmail.com>
2017-11-23 12:15:46 -0700

    Made some changes

--- Swift fun/ViewController.swift
+++ Swift fun/ViewController.swift
@@ -4,15 +4,29 @@
 //
 //  Created by Jackson Isiko on 11/22/17.
 //  Copyright © 2017 YOITPRO. All rights reserved.
-//
+// Hello
 
 import UIKit
 
 class ViewController: UIViewController {
+    var buttonCount = 0
+    
 
+    @IBOutlet weak var myLable: UILabel!
+    @IBAction func buttonTapped(_ sender: Any) {
+        buttonCount = buttonCount + 1
+        print (buttonCount)
+        if buttonCount >= 10 {
+            view.backgroundColor = UIColor.red
+            myLable.text = "Jackson Isiko"
+        }
+       
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
