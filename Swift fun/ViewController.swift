429ae150e441a2bef810365c42860da06b7f798c
Unknown <isijackson@gmail.com>
2017-11-23 22:44:51 -0700

    Emoji

--- Swift fun/ViewController.swift
+++ Swift fun/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var buttonCount = 0
+    //var buttonCount = 0
     
 
     @IBOutlet weak var myLable: UILabel!
+    @IBOutlet weak var toTextfeild: UITextField!
+    @IBOutlet weak var bottomTextfeild: UITextField!
     @IBAction func buttonTapped(_ sender: Any) {
-        buttonCount = buttonCount + 1
+        
+        //print(toTextfeild.text!)
+        
+        
+        //print(bottomTextfeild.text!)
+        
+       
+        /*buttonCount = buttonCount + 1
         //printing information
         print (buttonCount)
     
@@ -22,9 +31,28 @@ class ViewController: UIViewController {
             view.backgroundColor = UIColor.red
             myLable.text = "Jackson Isiko"
         }
-       
+       */
+        let addition = additionSwtich.isOn
+        if addition {
+            let sum = Double(toTextfeild.text!)! - Double(bottomTextfeild.text!)!
+            
+            //myLable.text = String(sum)
+            // using string interpolation
+            myLable.text = "The Sum is ...\(sum)"
+            
+            
+        } else{
+            let sum = Double(toTextfeild.text!)! + Double(bottomTextfeild.text!)!
+            
+            //myLable.text = String(sum)
+            // using string interpolation
+            myLable.text = "The Sum is ...\(sum)"
+        }
+        
     }
+    @IBOutlet weak var additionSwtich: UISwitch!
     
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -35,7 +63,10 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+// string interpolation
+    // myLable.text = " This is " + String(sum)
+    // or
+    
 
 }
 
